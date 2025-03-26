cfcb0998060adc2d051d1aad92b8b0ee56d78c8c
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2025-03-26 17:21:07 +0100

    file-modification: added method to perform statistical analysis of movies

--- main.py
+++ main.py
@@ -59,6 +59,23 @@ class MovieAnalyzer:
         df_movies = pd.DataFrame(movies)
         return df_movies[['title', 'vote_average', 'popularity', 'original_language']]
 
+    def statistical_analysis(self, df):
+        """
+        Performs statistical analysis of movies
+
+        Args:
+            df (pandas.DataFrame): DataFrame with movies
+
+        Returns:
+            dict: Dictionary with key statistics
+        """
+        statistics = {
+            'average_rating': df['vote_average'].mean(),
+            'popularity_median': df['popularity'].median(),
+            'languages': df['original_language'].value_counts()
+        }
+        return statistics
+
 
 if __name__ == "__main__":
     analyzer = MovieAnalyzer()
